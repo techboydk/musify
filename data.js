92ad240030ac5e86bd87359d03e60d77ddc9d4d0
Dipesh Yadav <73176903+techboydk@users.noreply.github.com>
2022-02-24 02:31:49 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,49 +1,49 @@
 let songs = [{
         name: 'song 1',
-        path: 'assets/musics/Song 1.mp3',
+        path: '/musify/assets/musics/Song 1.mp3',
         artist: 'artist 1',
-        cover: 'assets/images/cover 1.png'
+        cover: '/musify/assets/images/cover 1-min.png'
     },
     {
         name: 'song 2',
-        path: 'assets/musics/Song 2.mp3',
+        path: '/musify/assets/musics/Song 2.mp3',
         artist: 'artist 2',
-        cover: 'assets/images/cover 2.png'
+        cover: '/musify/assets/images/cover 2-min.png'
     },
     {
         name: 'song 3',
-        path: 'assets/musics/Song 3.mp3',
+        path: '/musify/assets/musics/Song 3.mp3',
         artist: 'artist 3',
-        cover: 'assets/images/cover 3.png'
+        cover: '/musify/assets/images/cover 3-min.png'
     },
     {
         name: 'song 4',
-        path: 'assets/musics/Song 4.mp3',
+        path: '/musify/assets/musics/Song 4.mp3',
         artist: 'artist 4',
-        cover: 'assets/images/cover 4.png'
+        cover: '/musify/assets/images/cover 4-min.png'
     },
     {
         name: 'song 5',
-        path: 'assets/musics/Song 5.mp3',
+        path: '/musify/assets/musics/Song 5.mp3',
         artist: 'artist 5',
-        cover: 'assets/images/cover 5.png'
+        cover: '/musify/assets/images/cover 5-min.png'
     },
     {
         name: 'song 6',
-        path: 'assets/musics/Song 6.mp3',
+        path: '/musify/assets/musics/Song 6.mp3',
         artist: 'artist 6',
-        cover: 'assets/images/cover 6.png'
+        cover: '/musify/assets/images/cover 6-min.png'
     },
     {
         name: 'song 7',
-        path: 'assets/musics/Song 7.mp3',
+        path: '/musify/assets/musics/Song 7.mp3',
         artist: 'artist 7',
-        cover: 'assets/images/cover 7.png'
+        cover: '/musify/assets/images/cover 7-min.png'
     },
     {
         name: 'song 8',
-        path: 'assets/musics/Song 8.mp3',
+        path: '/musify/assets/musics/Song 8.mp3',
         artist: 'artist 8',
-        cover: 'assets/images/cover 8.png'
+        cover: '/musify/assets/images/cover 8-min.png'
     },
 ]
